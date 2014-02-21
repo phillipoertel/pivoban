4ccc656a7cbad70b40d2142c687ba23b8a105bb2
Phillip Oertel <me@phillipoertel.com>
2014-02-21 18:22:58 +0100

    Mina configured?

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, %(log config)
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
@@ -32,7 +32,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  invoke :'rvm:use[ruby-1.9.3@pivoban]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -44,9 +44,6 @@ task :setup => :environment do
 
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
-
-  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
 end
 
 desc "Deploys the current version to the server."
