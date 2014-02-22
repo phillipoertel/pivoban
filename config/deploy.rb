97c83baa074a473d09b66c48ba1c5f31c66deda5
Phillip Oertel <me@phillipoertel.com>
2014-02-22 11:17:58 +0100

    Simplified deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,6 @@
 require 'mina/bundler'
-#require 'mina/rails'
 require 'mina/git'
-require 'mina/rvm'    # for rvm support. (http://rvm.io)
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+require 'mina/rvm'
 
 set :user, 'root'
 set :domain, 'pivoban.torial.com'
@@ -19,24 +12,14 @@ set :branch, 'master'
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, %w(log config)
 
-# Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
   # For those using RVM, use this to load an RVM version@gemset.
   invoke :'rvm:use[ruby-1.9.3@pivoban]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
@@ -53,8 +36,6 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    #invoke :'rails:db_migrate'
-    #invoke :'rails:assets_precompile'
 
     queue! %[mkdir -p tmp]
 
