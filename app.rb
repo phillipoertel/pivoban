d0aa8a376bfd3c70332b85d3d7fe85075c4f789e
Phillip Oertel <me@phillipoertel.com>
2014-02-22 16:54:22 +0100

    added d3.js, nvd3.js and a working example.

--- app.rb
+++ app.rb
@@ -1,12 +1,21 @@
 require 'sinatra'
 require "slim"
+require "json"
 require "sinatra/reloader" if development?
 require "sinatra/config_file"
 
 # http://www.sinatrarb.com/contrib/config_file.html
 config_file 'config/settings.yml'
+set :slim, pretty: true
 
 get '/' do
-  settings.project_id
-  #slim :index
+  @settings = settings
+  slim :index
+end
+
+get '/bugs.json' do
+  content_type :json
+  File.read('public/stackedAreaData.json')
+  #[[1,2,3],[2,4,6]].to_json
+
 end
