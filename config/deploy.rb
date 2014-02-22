9a4cb7b7eaaf55f32ccbef16a32104dd596d4efc
Phillip Oertel <me@phillipoertel.com>
2014-02-22 18:19:25 +0100

    Don't link shared config path for now

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, %w(log config)
+set :shared_paths, %w(log)
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
